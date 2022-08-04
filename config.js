425f5130f0f0a00ee18bc7ce5b400fff35c480c4
Baov-1404 <110246958+Baov-1404@users.noreply.github.com>
2022-08-04 20:58:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Anh yêu em nhiều lắm Vy!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'Hôm nay sinh nhật chị đúng không?',
+    introDesc: `Em có tra google thì họ bảo
+    hôm nay hình như là ngày 
+    sinh nhật của chị`,
+    btnIntro: 'Kiểm tra',
+    title: 'Chị giúp em xác nhận lại đúng hay sai',
+    desc: 'Phải chăng em đoán đúng? ',
+    btnYes: 'Đúng rồi',
+    btnNo: 'Ai mượn đoán',
+    question:'2022-2005 thì ra số tuổi chị đó, chị tính thử đi',
+    btnReply: 'Bấm xác nhận',
+    reply: '17 tuổi',
+    mess: 'Nói chứ làm sao em quên sinh nhật chị được',
+    messDesc: 'Bắt đầu video cmsn nha',
+    btnAccept: 'Ừ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
